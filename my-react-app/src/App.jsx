7d57b30ac76de3df3bd44597e01aaeccf918bca8
José Silva <jg.desenvolver1@outlook.com>
2026-07-14 22:43:05 -0300

    José: Aula 1 - Fundamentos do React

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import Welcome from './components/Welcome';
+
+function App() {
+  return (
+    <div>
+      <Welcome name="José" />
+    </div>
+  );
+}
+
+export default App;
+          
