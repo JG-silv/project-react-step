319f1e9071b9126856ecd594671a0749f6f6cfdf
Fabio Luiz Daemon Da Silva <devfabioluiz@gmail.com>
2026-07-14 22:29:52 -0300

    aula 1 react fundamentos

---
+++ exemplo/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Welcome from "./components/Welcome";
+
+const App = () => {
+  return (
+    <div>
+      <Welcome name="Fabio" />
+      <p>outro texto</p>
+    </div>
+  );
+};
+
+export default App;
