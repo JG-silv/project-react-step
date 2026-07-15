0ca2242dcf408d0485f5dfc6330020bbad5e1c4d
Fabio Luiz Daemon Da Silva <devfabioluiz@gmail.com>
2026-07-14 21:53:04 -0300

    aula 1 react fundamentos

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import BoasVindas from './components/BoasVindas'
+
+function App() {
+  return (
+    <div>
+      <BoasVindas nome="Fabio" />
+    </div>
+  )
+}
+
+export default App
---
+++ my-react-app/src/components/BoasVindas.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const BoasVindas = ({ nome }) => {
+  return (
+    <div>
+      <h1>Bem-vindo, {nome}!</h1>
+    </div>
+  )
+}
+
+export default BoasVindas
