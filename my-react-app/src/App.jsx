fd33ffabf40a15113f66f4d484ddf3b44b78c0a1
José Silva <jg.desenvolver1@outlook.com>
2026-07-21 22:17:30 -0300

    comentário do commit

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import PlacarDoJogo from "./components/PlacarDoJogo";
+
+function App() {
+  return (
+    <div>
+      <PlacarDoJogo />
+    </div>
+  );
+}
+
+export default App;
