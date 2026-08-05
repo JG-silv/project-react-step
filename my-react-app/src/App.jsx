70a87c2807d69d4904abdefdef2807b9759ff6ae
José Silva <jg.desenvolver1@outlook.com>
2026-08-04 21:08:40 -0300

    José: Aula 06 React v2

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,6 +1,7 @@
 import ButtonCSS from './components/ButtonCSS';
 import ButtonInline from './components/ButtonInline';
 import ButtonStyled from './components/ButtonStyled';
+import TailwindButton from "./components/TailwindButton";
 
 //O App.jsx gerencia o estado isLoggedIn e altera o texto do botão com ternário: {isLoggedIn ? 'Logout' : 'Login'}. 
 // Quando o estado muda, tanto o App quanto o LoginStatus (que recebe a prop) são re-renderizados.
@@ -10,17 +11,16 @@ function App() {
     <div>
       <h1>Botão com CSS Externo</h1>
       <ButtonCSS label="CSS Externo" />
-
       <h1>Botão com Inline Styles</h1>
       <ButtonInline label="Primário" primary />
       <ButtonInline label="Secundário" />
-
       <h1>Botão com Styled Components</h1>
       <ButtonStyled label="Primário" primary />
       <ButtonStyled label="Secundário" />
+      <h1>Botão com Tailwind CSS</h1>
+      <TailwindButton label="Botão" />
     </div>
   );
 }
 
 export default App;
-          
