de479ce36595be326cd7474cd70d812c89e28a8c
José Silva <jg.desenvolver1@outlook.com>
2026-08-11 22:36:24 -0300

    José: dever 09 react

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,16 +1,11 @@
-import Usuarios from "./components/aula8/Usuarios";
-import Posts from "./components/aula8/Posts";
-import ListaUsuarios from "./components/aula8/ListaUsuarios";
-import PostList from "./components/aula8/PostList";
+import FeedbackForm from "./components/aula9/FeedbackForm";
+import RegistrationForm from "./components/aula9/RegistrationForm";
 
 function App() {
   return (
      <div>
-      <h1>JSONPlaceholder App</h1>
-      <Usuarios />
-      <Posts />
-      <ListaUsuarios />
-      <PostList />
+      <FeedbackForm />
+      <RegistrationForm />
     </div>
   );
 }
