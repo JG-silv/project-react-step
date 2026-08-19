01a9c1b6946f0cb0677d7cc9c48a93b84f9dbc57
José Silva <jg.desenvolver1@outlook.com>
2026-08-18 22:54:05 -0300

    José: Dever de casa 10 - Aula React

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,10 +1,10 @@
 
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import Home from './src/components/aula10/Home';
-import Login from './src/components/aula10/Login';
-import PrivateRoute from './src/components/aula10/PrivateRoute';
-import User from './src/components/aula10/User';
-import About from './src/components/aula10/About';
+import Home from './components/aula10/Home';
+import Login from './components/aula10/Login';
+import PrivateRoute from './components/aula10/PrivateRoute';
+import User from './components/aula10/User';
+import About from './components/aula10/About';
 import './App.css';
 
 const App = () => {
